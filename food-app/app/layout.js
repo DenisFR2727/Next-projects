65dfa53518e52e98d24aa47a51703d8eab60e329
Denys <dh92fr@gmail.com>
2025-06-18 13:17:39 +0300

    Add food-app project

---
+++ food-app/app/layout.js
@@ -0,0 +1,21 @@
+import Header from '@/components/main-header/main-header';
+import MainHeaderBackground from '@/components/main-header/main-header-background';
+
+import './globals.css';
+
+export const metadata = {
+    title: 'NextLevel Food',
+    description: 'Delicious meals, shared by a food-loving community.',
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en" suppressHydrationWarning={true}>
+            <body>
+                <MainHeaderBackground />
+                <Header />
+                <main className="main">{children}</main>
+            </body>
+        </html>
+    );
+}
