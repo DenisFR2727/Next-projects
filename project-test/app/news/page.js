51ab4961a3513c3e1d1648ff78718ac4590a0df7
Denys <dh92fr@gmail.com>
2025-06-26 19:22:08 +0300

    create slug page in slug folder and add styles in slug page

---
+++ project-test/app/news/page.js
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import { DUMMY_NEWS } from '@/dummy-new';
+import Image from 'next/image';
+
+import '../globals.css';
+
+export default function NewsPage() {
+    return (
+        <>
+            <h1>News:</h1>
+            <ul className="news-list">
+                {DUMMY_NEWS.map((newsItem) => (
+                    <li key={newsItem.id}>
+                        <Link href={`/news/${newsItem.slug}`}>
+                            <img
+                                src={`/images/news/${newsItem.image}`}
+                                alt={newsItem.title}
+                            />
+                        </Link>
+                        <span>{newsItem.title} </span>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+}
