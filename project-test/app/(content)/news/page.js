decd48c5facd1b4a83b54a9aa7cadeff68ba38e9
Denys <dh92fr@ukr.net>
2025-07-18 13:53:26 +0300

    new get data in NewsPage.js

--- project-test/app/(content)/news/page.js
+++ project-test/app/(content)/news/page.js
@@ -1,16 +1,13 @@
 import NewsList from '@/components/news-list';
+import { getAllNews } from '@/lib/news';
 // Звичайний і традиційний спосіб отримання данних на React - Не самий кращий варыант для отримання данних на NEXT js
+// bettre-sqlite3 - цей пакет нам дозволяє взаємодіяти з базою данних.
+// const news = getAllNews(); - Найкращий спосіб отримання данних якщо ми володіємо базою данних ( наприклад DB в додадтку)
 
 import '@/styles/globals.css';
 
 export default async function NewsPage() {
-    const response = await fetch(`http://localhost:8080/news`);
-
-    if (!response.ok) {
-        throw new Error(`feiled to fetch news.`);
-    }
-
-    const news = await response.json();
+    const news = getAllNews();
 
     return (
         <>
@@ -19,3 +16,11 @@ export default async function NewsPage() {
         </>
     );
 }
+
+//  const response = await fetch(`http://localhost:8080/news`);
+
+//     if (!response.ok) {
+//         throw new Error(`feiled to fetch news.`);
+//     }
+
+//     const news = await response.json();
