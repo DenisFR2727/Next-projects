8f24b2dc11fc36c16b228b57523d0105cdb728a7
Denis Leon <dh92fr@ukr.net>
2025-07-25 20:56:08 +0300

    add loading.tsx in products

--- products_shop/components/products/products-list.tsx
+++ products_shop/components/products/products-list.tsx
@@ -6,8 +6,8 @@ import { useGetProductsQuery } from "@/lib/features/products/api/productsApi";
 export default function ProductList() {
   const { data, isLoading, error } = useGetProductsQuery();
 
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error fetching products</p>;
+  //   if (isLoading) return <p>Loading...</p>;
+  //   if (error) return <p>Error fetching products</p>;
 
   return (
     <ul>{data?.map((product) => <li key={product.id}>{product.title}</li>)}</ul>
--- products_shop/lib/features/products/api/productsApi.ts
+++ products_shop/lib/features/products/api/productsApi.ts
@@ -1,14 +1,16 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { IProducts, IProductsResponse } from "@/lib/types";
 
-// Define a service using a base URL and expected endpoints
 export const productsApi = createApi({
   reducerPath: "productsApi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://dummyjson.com" }),
   endpoints: (build) => ({
     getProducts: build.query<IProducts[], void>({
       query: () => "/products",
-      transformResponse: (response: IProductsResponse) => response.products,
+      transformResponse: async (response: IProductsResponse) => {
+        await new Promise((res) => setTimeout(res, 3000));
+        return response.products;
+      },
     }),
   }),
 });
