c5b019c1f9a6db36eed4c20acb78597631470398
Denis Leon <dh92fr@ukr.net>
2025-08-04 18:14:29 +0300

    api commit

--- project-test/app/api/route.js
+++ project-test/app/api/route.js
@@ -1,10 +1,10 @@
 // Обробник маршрутів - це файл в який ми можему єкспортувати різні функції які повинні мати назви функцій GET POST PATCH PUT DELETE
 
 export function GET(request) {
-    console.log(request);
+  console.log(request);
 
-    // return new Response.json();
+  // return new Response.json()
 
-    return new Response('Hello!');
+  return new Response("Hello!");
 }
 // export function POST(request) {}
