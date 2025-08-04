9940aec9d0b5b4c25c534484aa44d365c06ba356
Denis Leon <dh92fr@ukr.net>
2025-08-05 01:19:12 +0300

    delete primise fake 2000ms loading page list products

--- products_shop/app/(content)/products/page.tsx
+++ products_shop/app/(content)/products/page.tsx
@@ -2,8 +2,6 @@ import ProductList from "@/components/products/products-list";
 import { getProducts } from "@/lib/products";
 
 export default async function ProductsPage() {
-  await new Promise((res) => setTimeout(res, 2000));
-
   const response = await getProducts();
   const data = response.products;
 
