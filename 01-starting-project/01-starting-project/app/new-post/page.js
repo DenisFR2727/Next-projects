d0e661cf810a816f5ef20f3aa7ae4cc041593374
Denis Leon <dh92fr@ukr.net>
2025-08-14 13:19:23 +0300

    createPost(formData)

--- 01-starting-project/01-starting-project/app/new-post/page.js
+++ 01-starting-project/01-starting-project/app/new-post/page.js
@@ -1,8 +1,26 @@
+import { storePost } from "@/lib/posts";
+
 export default function NewPostPage() {
+  async function createPost(formData) {
+    "use server";
+
+    const title = formData.get("title");
+    const image = formData.get("image");
+    const content = formData.get("content");
+
+    storePost({
+      imageUrl: "",
+      title,
+      content,
+      userId: 1,
+    });
+    console.log(title, image, content);
+  }
+
   return (
     <>
       <h1>Create a new post</h1>
-      <form>
+      <form action={createPost}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
           <input type="text" id="title" name="title" />
