b1f897d89e6e0c6727872a1c98a8f3f4bc52de61
Denis Leon <dh92fr@ukr.net>
2025-08-14 13:56:04 +0300

    redirect(/feed);

--- 01-starting-project/01-starting-project/app/new-post/page.js
+++ 01-starting-project/01-starting-project/app/new-post/page.js
@@ -1,4 +1,5 @@
 import { storePost } from "@/lib/posts";
+import { redirect } from "next/navigation";
 
 export default function NewPostPage() {
   async function createPost(formData) {
@@ -8,12 +9,13 @@ export default function NewPostPage() {
     const image = formData.get("image");
     const content = formData.get("content");
 
-    storePost({
+    await storePost({
       imageUrl: "",
       title,
       content,
       userId: 1,
     });
+    redirect("/feed");
     console.log(title, image, content);
   }
 
