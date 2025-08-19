a00db7c370aa8df6c16d2f468d2e3b17dea487ef
Denis Leon <dh92fr@ukr.net>
2025-08-19 19:17:00 +0300

    add styles filter-panel

--- products_shop/components/products/filter/filter-panel.tsx
+++ products_shop/components/products/filter/filter-panel.tsx
@@ -9,6 +9,8 @@ import {
 } from "@/lib/features/products/filterProductsSlice";
 import { ProductListProps } from "../products-list";
 
+import "./filter-panel.scss";
+
 export default function FilterPanel({ products }: ProductListProps) {
   const dispatch = useAppDispatch();
   const categories = products.map((p) => p.category);
@@ -35,20 +37,21 @@ export default function FilterPanel({ products }: ProductListProps) {
   };
 
   return (
-    <div>
+    <div className="filter_panel">
       <form onSubmit={(e) => e.preventDefault()}>
         <div className="search_product">
           <label htmlFor="">Serch Product</label>
           <input
+            className="search_item-input"
             type="text"
             value={searchTitle}
             onChange={changeSearch}
-            placeholder="Search products..."
           />
         </div>
         <div className="select_category">
           <label>Select category</label>
           <select
+            className="select_item-category"
             value={selectedCategory}
             onChange={(e) => setSelectedCategories(e.target.value)}
           >
@@ -66,15 +69,20 @@ export default function FilterPanel({ products }: ProductListProps) {
             <p>{defaultRange}</p>
           </div>
           <input
+            className="range_price"
             type="range"
             max={3000}
             value={defaultRange}
             onChange={(e) => setDefaultRange(+e.target.value)}
           />
         </div>
-        <div className="filter-buttons">
-          <button onClick={submitSearch}>SEARCH</button>
-          <button onClick={resetValuesForm}>RESET</button>
+        <div className="filter_buttons">
+          <button className="search_btn-filter" onClick={submitSearch}>
+            SEARCH
+          </button>
+          <button className="reset_btn-filter" onClick={resetValuesForm}>
+            RESET
+          </button>
         </div>
       </form>
     </div>
