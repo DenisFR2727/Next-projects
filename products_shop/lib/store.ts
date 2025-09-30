930314fc034e8c91bf05ac60227fd15abf8e7f4b
Denis Leon <dh92fr@ukr.net>
2025-09-30 18:42:50 +0300

    blacklist store unsplash off

--- products_shop/lib/store.ts
+++ products_shop/lib/store.ts
@@ -9,7 +9,7 @@ import storage from "redux-persist/lib/storage";
 const persistConfig = {
   key: "root",
   storage,
-  blacklist: ["filterReducer", "paginationPage", "unsplashPage"],
+  blacklist: ["filterReducer", "paginationPage"],
 };
 
 const rootReducer = combineReducers({
