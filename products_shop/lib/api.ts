ef06f2862181631f415a5f0153c4496d361d729c
Denis Leon <dh92fr@ukr.net>
2025-10-07 17:46:33 +0300

    add cache force-cache

--- products_shop/lib/api.ts
+++ products_shop/lib/api.ts
@@ -4,16 +4,16 @@ import { IProducts, UnsPlash } from "./types";
 const UNSPLASH_ACCESS_KEY = process.env.UNSPLASH_ACCESS_KEY;
 
 export async function getProduct(id: number): Promise<IProducts | null> {
-  const res = await fetch(`https://dummyjson.com/products/${id}`);
+  const res = await fetch(`https://dummyjson.com/products/${id}`, {
+    cache: "force-cache",
+  });
   if (!res.ok) return null;
   return res.json();
 }
 
 export async function getProducts(): Promise<{ products: IProducts[] }> {
   const res = await fetch(`https://dummyjson.com/products`, {
-    next: {
-      revalidate: 10,
-    },
+    cache: "force-cache",
   });
   if (!res.ok) throw new Error("Failed to fetch products");
   return res.json();
